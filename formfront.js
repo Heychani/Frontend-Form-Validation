ce6df521f6cef26c040e515713a92e59be10d49f
Chanique Lombard <98527335+Heychani@users.noreply.github.com>
2022-06-06 11:22:23 +0200

    Adding some changes

--- formfront.js
+++ formfront.js
@@ -1,50 +1,58 @@
+const toDo = [];
+let textString;
+//console.log("document.getElementById('taskName') is: ");
+//console.log(document.getElementById("taskName"));
+//console.log("document.getElementById('taskName').value is: " + document.getElementById("taskName").value);
+let dateString;
 
-let toDo = (newtask);
+class task {
+    constructor(taskText, date) {
+        this._text = taskText;
+        this._date = date;
+    };
 
-
-//ek sukkel om die function en die label te link sodat die function weet ek praat van die labels in HTML
-function myValidation(text){
-    document.getElementById(newtask);//weet nie of ek even die getelement moet gebruik nie, maar dis al hoe ek dink om dit te connect
-    if (newtask == text){
-        return "You have successfully added a New Task!"; //ek wil he die task moet in gevul word, as daar nie iets ingevul is nie moet hy se dit moet ingevul word of well done dis ingevul
+    get getTask() {
+        return this._text;
     }
-    return "The task has not been filled in, please fill it in before submitting."
+};
+
+/*
+function task(taskText, date) {
+this.text = taskText;
+this.date = date;
+}
+*/
+
+function addTask() {
+    textString = document.getElementById("taskName").value;
+    //console.log("document.getElementById('taskName') is: ");
+    //console.log(document.getElementById("taskName"));
+    //console.log("document.getElementById('taskName').value is: " + document.getElementById("taskName").value);
+    dateString = document.getElementById("taskDate").value;
+    toDo.push(new task(textString, dateString));
+    displayTasks();
 }
 
-toDo.push(toDo);
-alert(myValidation(toDo));
+function displayTasks() {
+    for (let i = 0; toDo.length; i++) {
+        let myLi = document.createElement("li");
+        let taskT = document.createTextNode(toDo[i].getTask);
+        myLi.appendChild(taskT);
+        document.getElementById("myUl").appendChild(myLi);
+    }
+}
 
-// een kleur terwyl mens tik en dan 'n ander kleur wanneer dit ingesit is
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// const naam = "Chani";
-
-// function myValidation(){
-//     document.getElementById(newtask)="You've added a new task well done!"
-//   }
-
-// myValidation();
-
-//Alles van hier af boontoe is vir die To do app
-
-// function adder(num1, num2) {
-//   console.log(num1 + num2);
-// }
-
-// adder(2,4)
-
-// function toUpper(text){
-//   const upperCased = text.toUpperCase();
-//   console.log(upperCased);                             //van const naam tot const toUpper was net practice van functions en hoe dit werk.... 
-// }
-
-// toUpper(naam);
-
-// const toUpper = function(){
-//   "another way to write a function"
-// }
-
-// const toUpper = () => {
-//   "shortest way to write a function";
-// }
+//ek sukkel om die function en die label te link sodat die function weet ek praat van die labels in HTML
+function myValidation() {
+    let text = document.getElementById('newtask').value;
+    //console.log(text);
+    if (text !== '') {
+        return alert("You have successfully added a New Task!"); //ek wil he die task moet in gevul word, as daar nie iets ingevul is nie moet hy se dit moet ingevul word of well done dis ingevul
+    }
+    return alert("The task has not been filled in, please fill it in before submitting.");
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
